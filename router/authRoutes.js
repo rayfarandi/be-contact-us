03a70f0042d923221abed6a5f25305c2dac7abfd
rayfrandi <rayfrandi1994@gmail.com>
2025-03-23 12:29:40 +0700

    minor update

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe } = require('../controllers/authController');
+const { register, login, getUser } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 // Routes
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, getMe);
+router.get('/me', protect, getUser);
 
 module.exports = router;
